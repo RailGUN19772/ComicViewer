f5f90baadd9c59a1dfd9e3d8e4394b5431087a43
RailGUN19772 <windows8microsoft@yahoo.co.jp>
2016-12-13 22:01:14 +0900

    Comic Viewer Ver1.1

--- ComicViewer/ComicViewer/ComicViewer/SelectFile.cs
+++ ComicViewer/ComicViewer/ComicViewer/SelectFile.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
-using PCLStorage;
 
 using Xamarin.Forms;
 
--- ComicViewer/ComicViewer/ComicViewer/ver.cs
+++ ComicViewer/ComicViewer/ComicViewer/ver.cs
@@ -17,7 +17,7 @@ namespace ComicViewer
                 Spacing = 30,
                 Children = {
                     new Label
-                    { Text = "このアプリについて\n16/12/01 Ver1.0 Release\n16/12/12 Ver1.1 更新内容\n    ソースコード及び内部処理の簡略化\n    Aboutの追加\n    ボタン配置の変更及びサイズの調整" },
+                    { Text = "このアプリについて\n16/11/30 Ver1.0 Release\n16/12/12 Ver1.1 更新内容\n    ソースコード及び内部処理の簡略化\n    Aboutの追加\n    ボタン配置の変更及びサイズの調整" },
                     new Label
                     { Text = "動作確認端末及び環境\nSONY XPERIA Z5 Android6.0\nHUAWEI Media PAD 7 Lite Android4.0.3\n\nWindows10 AnniversaryUpdate"}
                 }
